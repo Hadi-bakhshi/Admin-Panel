6908ed89fa8aebe9209046dda4f6fb89488384dd
Hadi <hadibakhshi277@gmail.com>
2022-01-26 16:24:13 +0330

    Sidebar created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import TopBar from "./Components/TopBar/TopBar";
-
-
-
+import "./app.css";
+import SideBar from "./Components/SideBar/SideBar";
 
 function App() {
   return (
     <div>
-    <TopBar/>
+      <TopBar />
+      <div className="container">
+        <SideBar/>
+        <div className="others">Others</div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/SideBar/SideBar.jsx
@@ -0,0 +1,93 @@
+import "./sidebar.css";
+import {
+  LineStyle,
+  Timeline,
+  TrendingUp,
+  PersonOutline,
+  Storefront,
+  AttachMoney,
+  Equalizer,
+  MailOutline,
+  ChatBubbleOutline,
+  DynamicFeed,
+  ReportProblem,
+  WorkOutline,
+
+} from "@material-ui/icons";
+
+const SideBar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <LineStyle className="sidebarIcon" />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUp className="sidebarIcon" />
+              Sales
+            </li>
+          </ul>
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <PersonOutline className="sidebarIcon" />
+              Users
+            </li>
+            <li className="sidebarListItem">
+              <Storefront className="sidebarIcon" />
+              Products
+            </li>
+            <li className="sidebarListItem">
+              <AttachMoney className="sidebarIcon" />
+              Transactions
+            </li>
+            <li className="sidebarListItem">
+              <Equalizer className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <MailOutline className="sidebarIcon" />
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <ChatBubbleOutline className="sidebarIcon" />
+              Messages
+            </li>
+            <li className="sidebarListItem">
+              <DynamicFeed className="sidebarIcon" />
+              Feedback
+            </li>
+          </ul>
+          <h3 className="sidebarTitle">Stuff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <WorkOutline className="sidebarIcon" />
+              Manage
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <ReportProblem className="sidebarIcon" />
+              Report
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
